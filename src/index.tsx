582794a61da765b729e95309154bdd3fde63e86f
Airshock72 <gelagela2222@gmail.com>
2025-02-18 17:58:05 +0400

    added neccessary libraries to project

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+import { HashRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import App from 'src/App.tsx'
+
+const createElement = document.createElement('div')
+createElement.className = 'flex flex-col '
+createElement.id = 'root'
+document.body.appendChild(createElement)
+
+const domNode = document.getElementById('root') as HTMLDivElement
+const root = createRoot(domNode)
+
+root.render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+)
